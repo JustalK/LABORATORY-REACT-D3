9538e18115901b61c7a8d60a7e58bb72fdb3d0f8
Latsuj <justal.kevin@gmail.com>
2021-07-22 22:38:23 +0800

    [TEST] Adding the experience 1 for centering an image

--- README.md
+++ README.md
@@ -1,22 +1,23 @@
-# REACT APP TEMPLATE
+# LABORATORY REACT D3.js
 
 ## Goal
 
-This project is a template for building react-app with all my favorite tools already configured.
-
-Obviously, the app is using the latest version of `react`. For avoiding the relative path, I modified the webpack and allowing the import with an alias. I also added the support for sass files.
-
-In addition, I added `prettier` and configured it for working well with `esLint`. Finally, I added the basic usefull package such as the router and the helmet.  
+This project is my testing with the d3.js library
 
 ## Plan of the presentation
 
 I explain with all the details how I build the project and my way of working.
 
+- [Experiences](#experiences)
 - [Documentation](#documentation)
 - [Organization](#organization)
 - [Development](#development)
 - [Running](#running)
 - [Commands](#commands)
+
+## Experiences
+
+- **Experience1**: Testing to show an image in the center inside the svg
 
 ## Documentation
 #### Code documentation
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,35 +1,33 @@
 /**
- * The module managing the home page
- * @module Home
+ * Testing to center an image in the svg
+ * @module Experiences/Experience1
  */
 
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import * as d3 from 'd3'
 
 /**
- * @function Home
- * Create the component Home
- * @return {Object} Return the dom of the Home page
+ * @function Experience1
+ * Testing to center an image in the svg
+ * @return {Object} Return the dom
  */
-const Home = () => {
+const Experience1 = () => {
   const canvasRef = useRef()
+  const [size] = useState({ width: 500, height: 500 })
 
   useEffect(() => {
     const canvas = d3.select(canvasRef.current)
+    const { width, height } = canvasRef.current.getBoundingClientRect()
     canvas
       .append('image')
       .attr('xlink:href', './test.jpg')
-      .attr('width', 500)
-      .attr('height', 500)
-      .attr('x', 0)
-      .attr('y', 0)
+      .attr('width', size.width)
+      .attr('height', size.height)
+      .attr('x', (width - size.width) / 2)
+      .attr('y', (height - size.height) / 2)
   })
 
-  return (
-    <>
-      <svg className="canvas" ref={canvasRef}></svg>
-    </>
-  )
+  return <svg className="canvas" ref={canvasRef}></svg>
 }
 
-export default Home
+export default Experience1
