616439b366223c82fffe514c49c00dabfbb425be
Latsuj <justal.kevin@gmail.com>
2021-07-22 23:53:47 +0800

    [FEATURE] Adding a better explorer

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { ROUTE_EXPERIENCE_1, ROUTE_EXPERIENCE_2 } from '@constants/routes'
-import Experience1 from '@pages/Experience1'
-import Experience2 from '@pages/Experience2'
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom'
+import Experiences from '@pages'
 
 export default function App() {
   return (
     <Router>
-      <Switch>
-        <Route path={ROUTE_EXPERIENCE_2}>
-          <Experience2 />
-        </Route>
-        <Route path={ROUTE_EXPERIENCE_1}>
-          <Experience1 />
-        </Route>
-      </Switch>
+      <div className="navigation">
+        <nav>
+          <ul>
+            {Object.keys(Experiences).map((e, index) => (
+              <li key={index}>
+                <Link to={`/${index + 1}`}>{e}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+      <div className="content">
+        <Switch>
+          {Object.keys(Experiences).map((e, index) => {
+            const Type = Experiences[e]
+            return (
+              <Route key={index} path={`/${index + 1}`}>
+                <Type />
+              </Route>
+            )
+          })}
+          <Route path="/">
+            <span>Click on one of the experience on the left side</span>
+          </Route>
+        </Switch>
+      </div>
     </Router>
   )
 }
